eae42ececf24a0fe52d1045bdd9acf835b197f51
Mario Campos <mario-campos@github.com>
2021-02-22 16:27:34 -0600

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,11 @@
 int main(int argc, char argv[])
 {
+    // NON_COMPLIANT
     for (int i = 0, j = 1; i < j; i += 2) j++;
+    
+    // NON_COMPLIANT
+    int x, y;
+    for (x = 0, y = 1; x < y; x += 2) y++;
+    
     return 0;
 }

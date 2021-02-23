118a06fee4eaf5217a10dc905f9f8c510024dd1e
Mario Campos <mario-campos@github.com>
2021-02-23 13:12:33 -0600

    Test A6-5-4

--- hello.cpp
+++ hello.cpp
@@ -19,11 +19,23 @@ int main(int argc, char argv[])
     for (int i = 0; i < n; n--) { // NON_COMPLIANT
     }
     
-    for (auto i = init(); i < 10; inc(i)) { // COMPLIANT
+    for (auto i = init(); i < 10; inc(i)) { // NON_COMPLIANT
     }
     
     for (int i = 0; i < 10; i = do_foo(), i+1) { // NON_COMPLIANT
     }
+    
+    for (int i = 0; i < 10; i++); // COMPLIANT
+    
+    for (int i = 0; i < 10; ++i); // COMPLIANT
+    
+    for (int i = 10; i > 0; i--); // COMPLIANT
+    
+    for (int i = 10; i > 0; --i); // COMPLIANT
+    
+    for (int i = 0, j = 0; i != j; i++, j++); // COMPLIANT
+    
+    for (int i = 0; i < 10; i += 1); // COMPLIANT
     
     return 0;
 }

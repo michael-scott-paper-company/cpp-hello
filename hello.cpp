3ed08aba746845d292af691c51bb97ed4a6e8b87
Mario Campos <mario-campos@github.com>
2021-02-24 15:10:38 -0600

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,7 @@
-int main(int argc, char *argv[])
+#include <cstdint>
+#include <iterator>
+
+int main(int argc, char *argv[]) noexcept
 {
     constexpr std::int8_t arraySize = 7;
     std::uint32_t array[arraySize] = {0, 1, 2, 3, 4, 5, 6};
